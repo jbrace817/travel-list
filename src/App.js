b97361a668d6c07665c876e933ff82e2cd0f2f8a
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-07-08 11:50:54 -0400

    feat: added form elements and functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -12,11 +20,48 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 🥥</h1>;
 }
+
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
