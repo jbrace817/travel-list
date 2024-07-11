b3150ada1c91277f040b6e331cd96f19dbaa7fa2
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-07-11 14:21:44 -0400

    feat: added functionality to stats component
    stats component shows the number of items packed and a percentage derived from the items on the packing list.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -108,10 +108,28 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
     </div>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start Adding some items to your travel list ➕</em>
+      </p>
+    );
+  }
+
+  //Derived State
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((numItemsPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em> 💼 You have X items on your list, and you already packed X (x%) </em>
+      <em>
+        {' '}
+        {percentPacked === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `💼 You have ${numItems} items on your list, and you already packed        ${numItemsPacked} (${percentPacked}%)`}{' '}
+      </em>
     </footer>
   );
 }
